85b7dfba0e2d81d452f277b6d628211e20ec3b5d
wachiu <wachiuhk@gmail.com>
2018-07-04 00:52:04 +0800

    init

---
+++ main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"sort"
+
+	"github.com/coreos/go-semver/semver"
+	"github.com/google/go-github/github"
+)
+
+// LatestVersions returns a sorted slice with the highest version as its first element and the highest version of the smaller minor versions in a descending order
+func LatestVersions(releases []*semver.Version, minVersion *semver.Version) []*semver.Version {
+	versionSlice := make([]*semver.Version, len(releases))
+	copy(versionSlice, releases)
+	sort.Slice(versionSlice, func(i, j int) bool {
+		result := versionSlice[i].Compare(*versionSlice[j])
+		if result == 1 {
+			return true
+		}
+		return false
+	})
+	return versionSlice
+}
+
+func main() {
+	// Github
+	client := github.NewClient(nil)
+	ctx := context.Background()
+	opt := &github.ListOptions{PerPage: 10}
+	releases, _, err := client.Repositories.ListReleases(ctx, "kubernetes", "kubernetes", opt)
+	if err != nil {
+		panic(err) // TODO:is this really a good way?
+	}
+	minVersion := semver.New("1.8.0")
+	allReleases := make([]*semver.Version, len(releases))
+	for i, release := range releases {
+		versionString := *release.TagName
+		if versionString[0] == 'v' {
+			versionString = versionString[1:]
+		}
+		allReleases[i] = semver.New(versionString)
+	}
+	versionSlice := LatestVersions(allReleases, minVersion)
+
+	fmt.Printf("latest versions of kubernetes/kubernetes: %s", versionSlice)
+}
